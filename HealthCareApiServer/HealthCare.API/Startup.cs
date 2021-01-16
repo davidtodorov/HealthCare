6348c006dd7bb98a58544c87601eb42fd466c56a
David Todorov <t_david@outlook.com>
2021-01-16 16:49:06 +0200

    deployed to another azure profile

--- HealthCareApiServer/HealthCare.API/Startup.cs
+++ HealthCareApiServer/HealthCare.API/Startup.cs
@@ -1,5 +1,6 @@
 using HealthCare.API.Models;
 using HealthCare.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,9 +11,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HealthCare.API
@@ -37,6 +40,29 @@ namespace HealthCare.API
                 .AddIdentity<User, IdentityRole>()
                 .AddEntityFrameworkStores<HealthCareDbContext>();
 
+            var appSettingsConfiguration = Configuration.GetSection("ApplicationSettings");
+            services.Configure<ApplicationSettings>(appSettingsConfiguration);
+
+            var appSettings = appSettingsConfiguration.Get<ApplicationSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
             services.AddControllers();
         }
 
@@ -51,6 +77,13 @@ namespace HealthCare.API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            //app.UseCors(options => options
+            //    .AllowAnyOrigin()
+            //    .AllowAnyHeader()
+            //    .AllowAnyMethod()
+            //);
+
+            app.UseCors();
 
             app.UseAuthentication();
             app.UseAuthorization();
