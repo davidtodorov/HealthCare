8488bb274594389d90f38f57fb75a20a6dfa89e5
David Todorov <t_david@outlook.com>
2021-02-25 16:54:08 +0200

    Added login and register components

---
+++ health-care-app/src/components/authentication/AuthenticationForm.vue
@@ -0,0 +1,50 @@
+<template>
+	<v-container class="fill-height" fluid>
+		<v-row align="center" justify="center">
+			<v-col cols="12" sm="8" md="4">
+				<v-card class="elevation-12">
+					<v-toolbar color="#22AA94" dark flat>
+						<v-toolbar-title>{{title}}</v-toolbar-title>
+						<v-spacer />
+					</v-toolbar>
+					<v-card-text>
+						<slot></slot>
+					</v-card-text>
+					<v-card-actions>
+						<v-spacer />
+						<v-btn 
+							color="#22AA94" 
+							class="white--text"
+							:disabled=!allFieldsAreValid
+							@click="submit"
+						>{{title}}</v-btn>
+					</v-card-actions>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			requred: true
+		},
+		submitHandler: {
+			type: Function,
+			required: true
+		},
+		allFieldsAreValid: {
+			type: Boolean,
+			required: true
+		}
+	},
+	methods:{
+		submit() {
+			this.submitHandler()
+		}
+	}
+};
+</script>
---
+++ health-care-app/src/components/authentication/Login.vue
@@ -0,0 +1,55 @@
+<template>
+	<AppAuthenticationForm title="Login" :submitHandler="submitLogin" :allFieldsAreValid="true">
+		<v-form>
+			<v-text-field 
+				label="Email" 
+				name="email" 
+				type="text" 
+				color="#232323" 
+				v-model="email" 
+			/>
+
+			<v-text-field
+				id="password"
+				label="Password"
+				name="password"
+				type="password"
+				color="#232323"
+				v-model="password"
+			/>
+			<v-alert v-if="errorMessage" icon="far fa-user" color="red" text outlined>{{errorMessage}}</v-alert>
+		</v-form>
+	</AppAuthenticationForm>
+</template>
+
+<script>
+import AppAuthenticationForm from "./AuthenticationForm";
+
+export default {
+	components: {
+		AppAuthenticationForm
+	},
+	data: () => {
+		return {
+			email: "",
+			password: "",
+			errorMessage: ""
+		};
+	},
+	methods: {
+		submitLogin() {
+			// this.$store
+			// 	.dispatch("user/login", {
+			// 		email: this.email,
+			// 		password: this.password
+			// 	})
+			// 	.then(() => {
+			// 		this.$router.push({ name: "ProductList" });
+			// 	})
+			// 	.catch(err => {
+			// 		this.errorMessage = err.response.data;
+			// 	});
+		}
+	},
+};
+</script>
