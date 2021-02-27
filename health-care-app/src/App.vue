8468f3098b2ebd177e05829100e89e83bfba184a
David Todorov <t_david@outlook.com>
2021-02-27 15:15:21 +0200

    Change Login form title

--- health-care-app/src/App.vue
+++ health-care-app/src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <!-- <Login/> -->
-      <Register/>
+      <Login/>
+      <!-- <Register/> -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-// import Login from './components/authentication/Login';
-import Register from './components/authentication/Register';
+import Login from './components/authentication/Login';
+// import Register from './components/authentication/Register';
 
 export default {
   name: 'App',
 
   components: {
-    // Login,
-    Register
+    Login,
+    // Register
   },
 
   data: () => ({
