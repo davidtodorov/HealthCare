287fbb013980d5ddb399d6fa4391ecb72a6a839e
David Todorov <t_david@outlook.com>
2021-02-28 17:44:27 +0200

    add navigation

--- health-care-app/src/App.vue
+++ health-care-app/src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <v-app>
-    <v-main>
-      <Login/>
-      <!-- <Register/> -->
-    </v-main>
-  </v-app>
+	<v-app id="inspire">
+		<v-navigation-drawer v-model="drawer" app>
+			<v-card class="mx-auto" max-width="500">
+				<v-list>
+					<v-list-item-group>
+						<v-list-item v-for="(item, i) in items" :key="i" :to="item.to">
+							<v-list-item-icon>
+								<v-icon v-text="item.icon"></v-icon>
+							</v-list-item-icon>
+							<v-list-item-content>
+								<v-list-item-title
+									v-text="item.text"
+								></v-list-item-title>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list-item-group>
+          
+				</v-list>
+			</v-card>
+		</v-navigation-drawer>
+
+		<v-app-bar app>
+			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+			<v-toolbar-title>Application</v-toolbar-title>
+		</v-app-bar>
+
+		<v-main>
+			<router-view />
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import Login from './components/authentication/Login';
+// import Login from './components/authentication/Login';
 // import Register from './components/authentication/Register';
 
 export default {
-  name: 'App',
+	name: "App",
 
-  components: {
-    Login,
-    // Register
-  },
+	components: {
+		// Login,
+		// Register
+	},
 
-  data: () => ({
-    //
-  }),
+	data: () => ({
+		drawer: null,
+		items: [
+      {
+				icon: "mdi-inbox",
+				text: "Home",
+        to: "/home"
+			},
+			{
+				icon: "mdi-inbox",
+				text: "Lgoin",
+        to: "/login"
+			},
+			{
+				icon: "mdi-star",
+				text: "Register",
+        to: "/register"
+			},
+		],
+	}),
 };
 </script>
--- health-care-app/src/router/index.js
+++ health-care-app/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../components/authentication/Login'
+import Register from '@/components/authentication/Register'
 
 Vue.use(VueRouter)
 
@@ -8,7 +10,25 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/home',
+    name: 'Home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
   },
   {
     path: '/about',
