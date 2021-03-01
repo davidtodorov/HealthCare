a39c4314e1f1c92cb4b7984b3bf55a15ef46cb2b
David Todorov <t_david@outlook.com>
2021-03-01 19:37:10 +0200

    env variables test

--- health-care-app/src/App.vue
+++ health-care-app/src/App.vue
@@ -4,7 +4,11 @@
 			<v-card class="mx-auto" max-width="500">
 				<v-list>
 					<v-list-item-group>
-						<v-list-item v-for="(item, i) in items" :key="i" :to="item.to">
+						<v-list-item
+							v-for="(item, i) in items"
+							:key="i"
+							:to="item.to"
+						>
 							<v-list-item-icon>
 								<v-icon v-text="item.icon"></v-icon>
 							</v-list-item-icon>
@@ -15,7 +19,6 @@
 							</v-list-item-content>
 						</v-list-item>
 					</v-list-item-group>
-          
 				</v-list>
 			</v-card>
 		</v-navigation-drawer>
@@ -23,7 +26,7 @@
 		<v-app-bar app>
 			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-			<v-toolbar-title>Application</v-toolbar-title>
+			<v-toolbar-title>{{ url }}</v-toolbar-title>
 		</v-app-bar>
 
 		<v-main>
@@ -47,22 +50,24 @@ export default {
 	data: () => ({
 		drawer: null,
 		items: [
-      {
+			{
 				icon: "mdi-inbox",
 				text: "Home",
-        to: "/home"
+				to: "/home",
 			},
 			{
 				icon: "mdi-inbox",
 				text: "Lgoin",
-        to: "/login"
+				to: "/login",
 			},
 			{
 				icon: "mdi-star",
 				text: "Register",
-        to: "/register"
+				to: "/register",
 			},
 		],
+		url: process.env
 	}),
+	
 };
 </script>
